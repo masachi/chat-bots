fe064058a4f617ff6d0d85a2330bb684b6e99ead
マサチ <zhangjb42@gmail.com>
2020-02-09 16:22:07 +0800

    fix

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import ApiNotExistedException from "./exception/ApiNotExistedException";
 import {SullaService} from "./services/SullaService";
 
-// const Koa = require('koa');
-// const app = new Koa();
-// const json = require('koa-json');
-// const bodyParser = require('koa-bodyparser');
+const Koa = require('koa');
+const app = new Koa();
+const json = require('koa-json');
+const bodyParser = require('koa-bodyparser');
 // const routes = require('./routes');
 // const requestHandler = require('./middleware/RequestHandler');
 // const responseHandler = require('./middleware/ResponseHandler');
@@ -17,18 +17,18 @@ sulla.create().then(client => {
 });
 
 
-// app.use(bodyParser());
-// app.use(json());
-//
-// app.use(requestHandler);
-// app.use(responseHandler);
+app.use(bodyParser());
+app.use(json());
+
+app.use(requestHandler);
+app.use(responseHandler);
 //
 // app.use(routes());//注册路由
 //
-// app.use(async (ctx, next) => {
-//     throw new ApiNotExistedException();
-// });
-//
-// let port = process.env.PORT || 3030;
-// let server = app.listen(port);
+app.use(async (ctx, next) => {
+    throw new ApiNotExistedException();
+});
+
+let port = process.env.PORT || 3030;
+let server = app.listen(port);
 
